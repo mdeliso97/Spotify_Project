f387c41935adbb76a6da88285bd1e5aa9278c0c2
mdeliso97 <marco.deliso@students.unibe.ch>
2023-03-19 21:31:33 +0100

    added small changes

--- Algorithm_Clustering/LouvainAlgorithm.py
+++ Algorithm_Clustering/LouvainAlgorithm.py
@@ -37,4 +37,5 @@ for com in set(partition.values()):
     list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
     nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=str(count / size))
 nx.draw_networkx_edges(G, pos, alpha=0.5)
+plt.savefig('Louvain_Network.png')
 plt.show()
