33804e1ed34224edd880ae708e31317499764968
carattj <114411874+jack-unibe@users.noreply.github.com>
2023-03-19 11:56:12 +0100

    main python file
    Central file calling auxiliary python files

--- spotify_featuring_artists_analysis/main.py
+++ spotify_featuring_artists_analysis/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from data_reduction import *
+from data_expansion import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # path to data
+    nodes_path = '/Users/caratja/Desktop/Benefri/Semester2/Social Media Analytics/Project/Data/nodes.csv'
+    edges_path = '/Users/caratja/Desktop/Benefri/Semester2/Social Media Analytics/Project/Data/edges.csv'
+
+    # import data
+    nodes = pd.read_csv(nodes_path)
+    edges = pd.read_csv(edges_path)
+
+    # drop duplicates
+    nodes, edges = drop_duplicates(nodes, edges)
+
+    # remove artists with few featuring
+    n_featuring = 10  # minimum number of featuring we want to consider
+    artists_to_keep, _ = get_artists_more_than_n_featuring(edges, n_featuring)
+    nodes = filter_nodes(nodes, artists_to_keep)
+    edges = filter_edges(edges, artists_to_keep)
+
+    # add columns to nodes with number of featuring and hits
+    count_collaborations(nodes, edges)
+    count_chart_hits(nodes)
