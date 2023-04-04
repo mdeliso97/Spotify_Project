09836d8f64b2ba60a7e7b73cdfea450cc4442833
Arefay97 <127543670+Arefay97@users.noreply.github.com>
2023-04-04 12:09:00 +0200

    assigned id and color

--- Clustering/Pyvis.py
+++ Clustering/Pyvis.py
@@ -1,20 +1,19 @@
 import numpy as np
 import pandas as pd
-#from LouvainAlgorithm import *
 
-df = pd.read_csv("nodes_with_community.csv")
+df = pd.read_csv("communities.csv")
+print(df.head())
+communities = df.to_numpy().flatten()
+print(len(communities[0]))
 
-communties = df.community
-communties = communties.dropna()
 
-C_List = communties.tolist()
-#trying to extract each community nodes
+# trying to extract each community nodes
 '''Cluster_Nodes = []
 for j in range(len(C_List[0])):
     if C_List[0][j] not in Symbols :
-        Cluster_Nodes.append(C_List[0][j])'''
+        Cluster_Nodes.append(C_List[0][j])
 
-Symbols = ['{','}','[',']',' ',"'"]
+Symbols = ['{', '}', '[', ']', ' ', "'"]
 
 Cluster_Nodes = [C_List[0][j] for j in range(len(C_List[0])) if C_List[0][j] not in Symbols]
 
@@ -24,10 +23,9 @@ Cluster2 = [C_List[2][j] for j in range(len(C_List[2])) if C_List[2][j] not in S
 
 Cluster3 = [C_List[3][j] for j in range(len(C_List[3])) if C_List[3][j] not in Symbols]
 
-#print(Cluster_Nodes)
+# print(Cluster_Nodes)
 Nodes = ''.join(Cluster_Nodes)
 Nodes = Nodes.split(',')
-
 
 Nodes1 = ''.join(Cluster1)
 Nodes1 = Nodes1.split(',')
@@ -41,31 +39,24 @@ Nodes3 = Nodes1.split(',')
 community_id = pd.DataFrame(Nodes, columns=['community_id'])
 spotify_id = df['spotify_id']
 
+spotify_id = spotify_id.astype({'spotify_id': 'string'})
+community_id = community_id.astype({'community_id': 'string'})
 
-spotify_id = spotify_id.astype({'spotify_id':'string'})
-community_id = community_id.astype({'community_id':'string'})
-
-#print(community_id)
+# print(community_id)
 community_id_List = community_id['community_id'].values
 
+# map node Id to it's community id
+df['Community_id'] = df['spotify_id'].map(lambda x: 1 if x in community_id_List else 0)
 
-#map node Id to it's community id
-df['Community_id'] = df['spotify_id'].map(lambda x : 1 if x in community_id_List else 0)
-
-#print(df['Community_id'])
-#df.drop('community')
+# print(df['Community_id'])
+# df.drop('community')
 df.to_csv('nodes_with_community_id.csv')
 
-
-
-'''row = community_id.iloc[1]
-row1 = spotify_id.iloc[2]
-print("result is: " , row1 == row)
 #print(community_id['community_id'].head(10))
 
 #print(community_id.iloc[1])'''
 
-#concatenate
+# concatenate
 '''partitions = open("partition.txt", "r")
 
 partitions_list = partitions.readlines()
